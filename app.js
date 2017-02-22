a90005be85d1e997e2cf2f3d715d0394624800a1
Ang Cheng Siew <johnacsyen@gmail.com>
2017-02-22 15:48:34 +0800

    added login logic

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser(process.env.SESSION_SECRET))
 app.use(session({
   secret: process.env.SESSION_SECRET,
-  cookie: { maxAge: 60000 },
+  cookie: { maxAge: 600000 },
   resave: false,
   saveUninitialized: true,
   store: new MongoStore({
